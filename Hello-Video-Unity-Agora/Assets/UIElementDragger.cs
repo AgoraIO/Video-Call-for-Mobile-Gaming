830626ec4f320e82865816b271f6f439a5bc9cb9
Rick Cheng <rcsw.devel@gmail.com>
2019-11-21 17:23:05 -0800

    Draggable remote player display using RawImage; requires VideoSurface change.

---
+++ Hello-Video-Unity-Agora/Assets/UIElementDragger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIElementDragger : EventTrigger {
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        base.OnDrag(eventData);
+    }
+}
