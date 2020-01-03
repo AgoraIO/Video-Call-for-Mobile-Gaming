8987c3f32992d92992d410ed7c8f34c198410e09
Rick Cheng <rcsw.devel@gmail.com>
2020-01-02 16:13:21 -0800

    Added CanvasScalar custom version

---
+++ Hello-Video-Unity-Agora/Assets/AgoraEngine/Demo/CanvasScalerExt.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class CanvasScalerExt : CanvasScaler
+{
+
+    Vector2 LandSolution = new Vector2(1500, 2040); // y doesn't matter
+    Vector2 PortSolution = new Vector2(600, 400); // y doesn't matter
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+        if (Screen.width > Screen.height)
+        {
+            m_ReferenceResolution = LandSolution;
+        }
+        else
+        {
+            m_ReferenceResolution = PortSolution;
+        }
+    }
+}
