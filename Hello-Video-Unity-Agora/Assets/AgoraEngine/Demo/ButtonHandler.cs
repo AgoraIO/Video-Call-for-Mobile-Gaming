2a8d3bfccbb5dddf354828c94a96ba7555d9e64e
Rick Cheng <rcsw.devel@gmail.com>
2020-02-26 15:36:24 -0800

    Formatting update

--- Hello-Video-Unity-Agora/Assets/AgoraEngine/Demo/ButtonHandler.cs
+++ Hello-Video-Unity-Agora/Assets/AgoraEngine/Demo/ButtonHandler.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class ButtonHandler : MonoBehaviour
@@ -10,7 +8,8 @@ public class ButtonHandler : MonoBehaviour
     ///   React to a button click event.  Used in the UI Button action definition.
     /// </summary>
     /// <param name="button"></param>
-    public void onButtonClicked(Button button) {
+    public void onButtonClicked(Button button)
+    {
         // which GameObject?
         GameObject go = GameObject.Find("GameController");
         if (go != null)
